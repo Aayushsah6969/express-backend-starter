33517655243288356a9078b9161e5fd5215e96ae
Aayush Sah <aayushsah269@gmail.com>
2026-02-08 00:05:07 +0530

    fix: trigger semantic-release after permission update

--- release.config.js
+++ release.config.js
@@ -1,24 +1,25 @@
-export const branches = ["main"];
-export const plugins = [
+module.exports = {
+  branches: ["main"],
+
+  plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
 
-    [
-        "@semantic-release/changelog",
-        {
-            changelogFile: "CHANGELOG.md",
-        },
-    ],
+    ["@semantic-release/changelog", {
+      changelogFile: "CHANGELOG.md"
+    }],
 
     "@semantic-release/npm",
 
-    [
-        "@semantic-release/git",
-        {
-            assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
-            message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        },
-    ],
+    ["@semantic-release/git", {
+      assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
 
-    "@semantic-release/github",
-];
+    ["@semantic-release/github", {
+      successComment: false,
+      failComment: false,
+      labels: false
+    }]
+  ]
+};
